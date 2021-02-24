7df052afc5b5ed551c13ae48e90d9f5f0daf2ebf
Taylor Treece <taylor.treece@protonmail.com>
2021-02-24 13:50:23 -0600

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,19 @@ require 'date'
 require 'bundler/setup'
 Bundler.require
 
+task :environment do
+    require_relative './environment'
+  end
+
+task :console => :environment do
+    Pry.start
+end
+
+
 
 # put the code to connect to the database here
-
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/artists.sqlite"
+)
 require_relative "../artist.rb"
